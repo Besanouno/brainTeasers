3061896a0f6c125466ec43234ef6085fb38115c4
marcin <basistamarcin@gmail.com>
2016-10-20 19:28:53 +0200

    Added WordPuzzle interface

---
+++ src/main/java/pl/polpress/wordPuzzle/WordPuzzle.java
@@ -0,0 +1,10 @@
+package pl.polpress.wordPuzzle;
+
+import java.util.List;
+
+public interface WordPuzzle {
+	boolean insertWord(String word, int x, int y, Direction direction);
+	void fillEmptyFields();
+	List<Row> getBoardRows();
+	List<String> getWordsToFind();
+}
